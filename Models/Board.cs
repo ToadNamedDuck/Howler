9981f176ab1c3f7478afa9030c990e22b1f62be0
Jason Harris <lord.jtharris@gmail.com>
2023-07-26 10:19:17 -0500

    Added helpers to repository and the getallboards

--- Models/Board.cs
+++ Models/Board.cs
@@ -4,10 +4,10 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public int Topic { get; set; }
+        public string Topic { get; set; }
         public string Description { get; set; }
         public int BoardOwnerId { get; set; }
         public bool IsPackBoard { get; set; }
-        public User BoardOwner { get; set; }
+        public BarrenUser BoardOwner { get; set; }
     }
 }
--- Repositories/BoardRepository.cs
+++ Repositories/BoardRepository.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Howler.Models;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using Streamish.Repositories;
+using System.Collections.Generic;
 
 namespace Howler.Repositories
 {
@@ -16,5 +20,77 @@ namespace Howler.Repositories
         //Update Board
         //Delete a board.
         //GetBoardWithPosts - BoardWithPosts
+
+        public List<Board> GetAllBoards()//This does not, in fact, get all boards. But it gets all of the boards that AREN'T pack boards.
+            //The reason for this is because pack boards are PRIVATE spaces. If you're not in the pack, you should have as little information
+            //as possible about what goes on in their boards.
+        {
+            List<Board> boards = new List<Board>();
+            using(var connection = Connection)
+            {
+                connection.Open();
+
+                using(var cmd = connection.CreateCommand())
+                {
+                    cmd.CommandText = @"Select b.Id as bId, b.[Name], b.Topic, b.Description, b.BoardOwnerId, b.IsPackBoard,
+                                        u.Id as UserId, u.DisplayName, u.ProfilePictureUrl, u.DateCreated, u.PackId as userPackId, u.IsBanned
+                                        from Board b
+                                        Join [User] u
+                                        On b.BoardOwnerId = u.Id
+                                        Where IsPackBoard = 0";
+                    using(SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            boards.Add(BoardBuilder(reader));
+                        }
+                    }
+                }
+            }
+            return boards;
+        }
+        private Board BoardBuilder(SqlDataReader reader)
+        {
+            Board board = new()
+            {
+                Id = reader.GetInt32(reader.GetOrdinal("bId")),
+                Name = reader.GetString(reader.GetOrdinal("Name")),
+                Topic = reader.GetString(reader.GetOrdinal("Topic")),
+                Description = reader.GetString(reader.GetOrdinal("Description")),
+                BoardOwnerId = reader.GetInt32(reader.GetOrdinal("BoardOwnerId")),
+                IsPackBoard = reader.GetBoolean(reader.GetOrdinal("IsPackBoard")),
+                BoardOwner = UserBuilder(reader)
+            };
+            return board;
+        }
+        private BarrenUser UserBuilder(SqlDataReader reader)
+        {
+            BarrenUser user = new()
+            {
+                Id = reader.GetInt32(reader.GetOrdinal("UserId")),
+                DisplayName = reader.GetString(reader.GetOrdinal("DisplayName")),
+                DateCreated = reader.GetDateTime(reader.GetOrdinal("DateCreated")),
+                IsBanned = reader.GetBoolean(reader.GetOrdinal("IsBanned"))
+            };
+            if (!reader.IsDBNull(reader.GetOrdinal("userPackId")))
+            {
+                user.PackId = reader.GetInt32(reader.GetOrdinal("userPackId"));
+            }
+            else
+            {
+                user.PackId = null;
+            }
+
+            if (!reader.IsDBNull(reader.GetOrdinal("ProfilePictureUrl")))
+            {
+                user.ProfilePictureUrl = reader.GetString(reader.GetOrdinal("ProfilePictureUrl"));
+            }
+            else
+            {
+                user.ProfilePictureUrl = null;
+            }
+
+            return user;
+        }
     }
 }
