3301e6df798d6e0b6c3bd170cc81484a76efef74
Jason Harris <lord.jtharris@gmail.com>
2023-07-26 14:54:13 -0500

    Added the first endpoint for boards. Get

---
+++ Controllers/BoardsController.cs
@@ -0,0 +1,37 @@
+﻿using Howler.Models;
+using Howler.Repositories;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Security.Claims;
+
+namespace Howler.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    [Authorize]
+    public class BoardsController : Controller
+    {
+        private readonly IBoardRepository _boardRepository;
+        private readonly IUserRepository _userRepository;
+
+        public BoardsController(IBoardRepository boardRepository, IUserRepository userRepository)
+        {
+            _boardRepository = boardRepository;
+            _userRepository = userRepository;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            List<Board> boards = _boardRepository.GetAllBoards();
+            return Ok(boards);
+        }
+
+        private User GetCurrentUser()
+        {
+            var firebaseUserId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            return _userRepository.GetByFirebaseId(firebaseUserId);
+        }
+    }
+}
