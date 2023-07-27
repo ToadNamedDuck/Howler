cf62421c653f0f2add8fc3b7128a6999ea6387ed
Jason Harris <lord.jtharris@gmail.com>
2023-07-27 09:44:48 -0500

    Tagged BoardWithPosts with required and stringlength attributes

--- Models/Board.cs
+++ Models/Board.cs
@@ -1,11 +1,20 @@
-﻿namespace Howler.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Howler.Models
 {
     public class Board
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(75)]
         public string Name { get; set; }
+        [Required]
+        [StringLength(255)]
         public string Topic { get; set; }
+        [Required]
+        [StringLength(255)]
         public string Description { get; set; }
+        [Required]
         public int BoardOwnerId { get; set; }
         public bool IsPackBoard { get; set; }
         public BarrenUser BoardOwner { get; set; }
