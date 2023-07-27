b42c35648c1de366a17784006996cacbd27f237f
Jason Harris <lord.jtharris@gmail.com>
2023-07-27 09:47:24 -0500

    Last commit was supposed to say board and not boardwithposts. thats this commit. still tagged.

--- Models/BoardWithPosts.cs
+++ Models/BoardWithPosts.cs
@@ -1,13 +1,21 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Howler.Models
 {
     public class BoardWithPosts
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(75)]
         public string Name { get; set; }
+        [Required]
+        [StringLength(255)]
         public string Topic { get; set; }
+        [Required]
+        [StringLength(255)]
         public string Description { get; set; }
+        [Required]
         public int BoardOwnerId { get; set; }
         public bool IsPackBoard { get; set; }
         public BarrenUser BoardOwner { get; set; }
