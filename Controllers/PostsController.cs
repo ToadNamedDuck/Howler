96744c3ed8a4317d60f3e85f196d222d7ad24251
Jason Harris <lord.jtharris@gmail.com>
2023-07-27 14:53:11 -0500

    GetById endpoint - though maybe I should make it private

--- Controllers/PostsController.cs
+++ Controllers/PostsController.cs
@@ -1,11 +1,58 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Howler.Models;
+using Howler.Repositories;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace Howler.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class PostsController : ControllerBase
     {
+        private readonly IBoardRepository _boardController;
+        private readonly IPostRepository _postRepository;
+        private readonly IUserRepository _userRepository;
+        private readonly IPackRepository _packRepository;
+
+        public PostsController(IPostRepository postRepository, IBoardRepository boardController, IUserRepository userRepository, IPackRepository packRepository)
+        {
+            _boardController = boardController;
+            _postRepository = postRepository;
+            _userRepository = userRepository;
+            _packRepository = packRepository;
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            User sender = GetCurrentUser();
+            Post post = _postRepository.GetById(id);
+            Board board = _boardController.GetById(post.BoardId);
+            if (board.IsPackBoard == true)
+            {
+                if (sender.PackId == null)
+                {
+                    return Forbid();
+                }
+
+                Pack senderPack = _packRepository.GetById((int)sender.PackId);
+
+                if(senderPack.PrimaryBoardId == board.Id)
+                {
+                    return Ok(post);
+                }
+                return Forbid();
+            }
+            return Ok(post);
+        }
+
+        private User GetCurrentUser()
+        {
+            var firebaseUserId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            return _userRepository.GetByFirebaseId(firebaseUserId);
+        }
     }
 }
