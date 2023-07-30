0f3d52e0974f1dd4827ceda64786ee52951ce0ff
Jason Harris <lord.jtharris@gmail.com>
2023-07-29 19:58:57 -0500

    Added post to comment controller

--- Controllers/CommentsController.cs
+++ Controllers/CommentsController.cs
@@ -1,7 +1,10 @@
-﻿using Howler.Repositories;
+﻿using Howler.Models;
+using Howler.Repositories;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Security.Claims;
 
 namespace Howler.Controllers
 {
@@ -30,6 +33,48 @@ namespace Howler.Controllers
         public IActionResult Search(string q, bool latestFirst)
         {
             return Ok(_commentRepository.Search(q, latestFirst));
+        }
+
+        [HttpPost]
+        public IActionResult Post(Comment comment)
+        {
+            comment.CreatedOn = DateTime.Now;
+
+            if (String.IsNullOrWhiteSpace(comment.Content))
+            {
+                return BadRequest();
+            }
+            User sender = GetCurrentUser();
+
+            Post commentedOn = _postRepository.GetById(comment.PostId);
+            if (commentedOn == null || comment.UserId != sender.Id)
+            {
+                return BadRequest();
+            }
+
+            Board postBoard = _boardRepository.GetById(commentedOn.BoardId);
+            if (postBoard.IsPackBoard)
+            {
+                if(sender.PackId == null)
+                {
+                    return Unauthorized();
+                }
+                Pack senderPack = _packRepository.GetById((int)sender.PackId);
+
+                if (senderPack.PrimaryBoardId == postBoard.Id)
+                {
+                    _commentRepository.Add(comment);
+                    return StatusCode(201, comment);
+                }
+                return Unauthorized();
+            }
+            _commentRepository.Add(comment);
+            return StatusCode(201, comment);
+        }
+        private User GetCurrentUser()
+        {
+            var firebaseUserId = User.FindFirst(ClaimTypes.NameIdentifier).Value;
+            return _userRepository.GetByFirebaseId(firebaseUserId);
         }
     }
 }
