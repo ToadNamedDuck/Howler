e0df36f110f769e58a1c5b0632bf7662035ad96d
Jason Harris <lord.jtharris@gmail.com>
2023-07-31 08:57:29 -0500

    Added the omni/search all endpoint, and a new model for it to return. Hits all 4 searches.

---
+++ Models/OmniSearchResult.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Howler.Models
+{
+    public class OmniSearchResult
+    {
+        public List<Pack> Packs { get; set; }
+        public List<Board> Boards { get; set; }
+        public List<Post> Posts { get; set; }
+        public List<Comment> Comments { get; set; }
+    }
+}
