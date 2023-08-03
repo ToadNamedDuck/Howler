4ae0e9a6f2e93824f611f6ef44c467df8dc7df5f
Jason Harris <lord.jtharris@gmail.com>
2023-08-03 10:09:05 -0500

    Styling the header and other minor css

--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -3,6 +3,7 @@ import { NavLink as RRNavLink } from "react-router-dom";
 import {
     Card,
   CardBody,
+  CardHeader,
   CardImg,
   Nav,
   NavItem,
@@ -16,33 +17,34 @@ export default function Header({ isLoggedIn, loggedInUser }) {
   return (
     <div className='fixed-top'>
         <Card>
-            <CardImg top src="/logo192.png" height="100px" width="auto"/>
+                <h1 style={{color:"var(--bs-warning)"}} className='text-center'>Howler</h1>
+
             <CardBody>
                 <Nav className="mr-auto justify-content-around" >
                     { /* When isLoggedIn === true, we will render the Home link */}
                     {isLoggedIn &&
                     <>
-                        <NavLink tag={RRNavLink} to="/">Home</NavLink>
-                        <NavLink tag={RRNavLink} to="/packs">View Packs</NavLink>
-                        <NavLink tag={RRNavLink} to="/boards">View Boards</NavLink>
-                        {loggedInUser === null ? "" : <NavLink tag={RRNavLink} to={`/users/${loggedInUser.id}`}>My Profile</NavLink>}
+                        <NavLink tag={RRNavLink} to="/"><h3>Home</h3></NavLink>
+                        <NavLink tag={RRNavLink} to="/packs"><h3>View Packs</h3></NavLink>
+                        <NavLink tag={RRNavLink} to="/boards"><h3>View Boards</h3></NavLink>
+                        {loggedInUser === null ? "" : <NavLink tag={RRNavLink} to={`/users/${loggedInUser.id}`}><h3>My Profile</h3></NavLink>}
                     </>
                     }
                     {isLoggedIn &&
                     <>
                         <NavItem>
                         <a aria-current="page" className="nav-link" 
-                            style={{ cursor: "pointer" }} onClick={logout}>Logout</a>
+                            style={{ cursor: "pointer" }} onClick={logout}><h3>Logout</h3></a>
                         </NavItem>
                     </>
                     }
                     {!isLoggedIn &&
                     <>
                         <NavItem>
-                        <NavLink tag={RRNavLink} to="/login">Login</NavLink>
+                        <NavLink tag={RRNavLink} to="/login"><h3>Login</h3></NavLink>
                         </NavItem>
                         <NavItem>
-                        <NavLink tag={RRNavLink} to="/register">Register</NavLink>
+                        <NavLink tag={RRNavLink} to="/register"><h3>Register</h3></NavLink>
                         </NavItem>
                     </>
                     }
